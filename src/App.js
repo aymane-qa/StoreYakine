3b4deb49469c013d06f2bdbf63c4c5b4e80a13a0
aymane-qa <aymane.qasid@e-polytechnique.ma>
2025-08-11 04:32:12 +0100

    Ajout merci.html

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './Landing';
 import Produit from './Produit';
-import Merci from './Merci';
+
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/produit" element={<Produit />} />
-        <Route path="/merci" element={<Merci />} />
+                    className="thumbnail"
       </Routes>
     </Router>
   );
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -429,7 +429,8 @@ description: `
                 className="form"
               >
                 <input type="hidden" name="_captcha" value="false" />
-                <input type="hidden" name="_next" value="https://yakinestoree.netlify.app/merci" />
+               <input type="hidden" name="_next" value="https://yakinestoree.netlify.app/merci.html" />
+
 
                 <input type="hidden" name="_subject" value={`Nouvelle commande: ${productData.title}`} />
                 <input type="hidden" name="Produit" value={productData.title} />
